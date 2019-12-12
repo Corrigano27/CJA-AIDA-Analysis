57d4b75513f26e637e40ec7aa6480a2a7c30ba29
Corrigano27 <corriganappleton@hotmail.com>
2019-12-12 14:20:10 +0000

    veto update pid test

--- analysisHistograms.cpp
+++ analysisHistograms.cpp
@@ -95,7 +95,7 @@ int analysisHistograms(std::string iName, std::string cutFile){
 													betaVeto = true;
 												}
 											}
-											AIDA plastic beta veto - placeholder numbers, to be revised
+											//AIDA plastic beta veto - placeholder numbers, to be revised
 											if((*beta).T - anc.TIME < 20e3 && (anc.ID == 35 || anc.ID == 36)){
 												if((*beta).T - anc.TIME > 10e3 && (anc.ID == 35 || anc.ID ==36)){
 													betaVeto = true;
