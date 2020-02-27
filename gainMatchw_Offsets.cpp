b4831a91a2fe537a97b474cc3cd34dee64904d32
Corrigano27 <corriganappleton@hotmail.com>
2020-02-27 14:05:05 +0000

    attempt at output fix

--- gainMatchw_Offsets.cpp
+++ gainMatchw_Offsets.cpp
@@ -600,21 +600,11 @@ int gainMatch(std::string iName){
 		}
 	}
 
-	outf << "#adc offsets calculated from files..."<<endl;
-	cout << "#adc offsets calculated from files..."<<endl;
-
 	for (int i = 0; i<24; i++){
 		for (int j = 0; j<64; j++){
 			outf << "adcOffsetLowEnergy" << '\t' << i+1 << '\t' << j << '\t' << OffsetChannel[i][j] << std::endl;
-			cout << "adcOffsetLowEnergy" << '\t' << i+1 << '\t' << j << '\t' << OffsetChannel[i][j] << std::endl;
 		}
 	}
-
-	outf <<""<<endl;
-	cout <<""<<endl;
-
-	outf << "#adc gains calculated from files..."<<endl;
-	cout << "#adc gains calculated from files..."<<endl;
 
 	for (int i = 0; i<24; i++){
 		for (int j = 0; j<64; j++){
