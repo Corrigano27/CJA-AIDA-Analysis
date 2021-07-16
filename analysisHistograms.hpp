4540059bfad36831d2e81ffd10e86c951ccf4c05
Corrigano27 <corriganappleton@hotmail.com>
2021-07-16 14:42:44 +0100

    header file with variables

---
+++ analysisHistograms.hpp
@@ -0,0 +1,44 @@
+#ifndef ANALYSISHISTOGRAMS_HPP
+#define ANALYSISHISTOGRAMS_HPP
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+    
+bool betaVeto;
+bool gammaVeto;
+bool In97gammaVeto;
+
+bool Ag95_160;
+bool Ag95_800_1000;
+bool Ag95_440;
+bool Ag95_511;
+bool Ag95_randomCheck;
+
+bool Ag96_470;
+bool Ag96_743;
+bool Ag96_1249;
+bool Ag96_isomer;
+bool Ag96_subtraction;
+bool Ag96_Random;
+
+double IndyGammaE;
+double GammaSumTemp;
+double GammaSumTempBg;
+double GammaSumPCount;
+double GammaSumPCountBg;
+double ProtonGammaSumTemp;
+double ProtonGammaSumTempBg;
+double Sn101Counter_Pk;
+double Sn101CounterBg_Pk;
+double Sn101Counter;
+double Sn101CounterBg;
+double Ag94_Peak_Counter;
+double Ag94_Peak_CounterBg;
+double gammaSubtract;
+
+uint8_t multix = 0;
+uint8_t multiy = 0;
+
+#endif
