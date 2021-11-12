238e058539de759e04f2b07ea8c30cbd4121a1b9
Corrigano27 <corriganappleton@hotmail.com>
2021-11-12 16:19:00 +0000

    trimmed down gamma stuff

--- analysisHistograms.hpp
+++ analysisHistograms.hpp
@@ -7,14 +7,6 @@
 #include <fstream>
     
 bool betaVeto;
-bool gammaVeto;
-bool In97gammaVeto;
-
-bool Ag95_160;
-bool Ag95_800_1000;
-bool Ag95_440;
-bool Ag95_511;
-bool Ag95_randomCheck;
 
 /*bool Ag96_470;
 bool Ag96_743;
@@ -22,21 +14,6 @@ bool Ag96_1249;
 bool Ag96_isomer;
 bool Ag96_subtraction;
 bool Ag96_Random;*/
-
-double IndyGammaE;
-double GammaSumTemp;
-//double GammaSumTempBg;
-double GammaSumPCount;
-double GammaSumPCountBg;
-double ProtonGammaSumTemp;
-//double ProtonGammaSumTempBg;
-double Sn101Counter_Pk;
-double Sn101CounterBg_Pk;
-double Sn101Counter;
-double Sn101CounterBg;
-double Ag94_Peak_Counter;
-double Ag94_Peak_CounterBg;
-double gammaSubtract;
 
 uint8_t multix = 0;
 uint8_t multiy = 0;
